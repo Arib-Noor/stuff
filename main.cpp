a602aec6df209a0d32e32b769e09415a1b6caeb1
Something <anoor39@myseneca.ca>
2026-02-09 17:07:46 +0000

    Lab4

--- main.cpp
+++ main.cpp
@@ -1,22 +1,29 @@
-//Lab 2 Task 10
-#include <iostream>
+//Lab 4 Task 1
+#include <iostream>  
 using namespace std;
-#include <cstring>
+/*
+* Add necessary include files, use std namespace, and create the skeleton of main function.
+* Declare an integer variable `size`, and a float* pointer `scores`, initialized to `nullptr`.
+* Ask the user to enter the number of students and store it in `size`.
+* Dynamically allocate memory for a float array of that size using the statement:
+  ```cpp
+   scores = new float[size];
+  ```
+* Print a message confirming that memory was allocated.
+* Initialize the array with fixed values (e.g., {85.5, 90.0, 78.5, 92.0, 88.0}).
+* Use a loop to display the values.
+* Free the memory using delete[].
 
+*/
 int main() {
-    // Declare and initialize the source array
-    char source[] = "HelloWorld!";
-    char destination[sizeof(source)];
-    int size = sizeof(source) / sizeof(source[0]);
-    for (int i = 0; i < size; i++) {
-        destination[i] = source[i];
-    }
-    cout << source << endl;
-    cout << destination << endl;
-
-   char array3[sizeof(source)];
-   strncpy(array3, source, sizeof(array3) - 1);
-   cout << "Array3: " << array3 << endl;
-
-    return 0;
+  int size;
+  float *scores = nullptr;
+  cout << "Enter an integer " << endl;
+  cin >> size;
+  scores = new float[size];
+  cout << "Memory allocated" << endl;
+  scores = {1.0F,1.0F};
+  for (int i = 0; i < size; i++) {  
+    cout << scores[i];
+  }
 }
