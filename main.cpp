8e3d13cc84ac2aaa84c3817a3814cfd0d7aca249
Something <anoor39@myseneca.ca>
2026-02-19 04:08:43 +0000

    work

--- main.cpp
+++ main.cpp
@@ -1,18 +1,44 @@
-//Lab 5 Task 2
+//Lab 5 Task 6
 #include <iostream>  
-using namespace std;
+using namespace std;  
+#include <cstring>
 
-void swap(int &a, int &b) {
-  int temp = a;
-  a = b;
-  b = temp;
+/*
+* Write a C++ program that allows the user to create a movie database. The program should:
+    1. Ask the user how many movies they want to add to the database.
+    2. Dynamically allocate memory for an array to store the movie names and an array to store the movie ratings.
+    3. Prompt the user to enter the name and rating (out of 10) for each movie.
+    4. Write a function that should receive the movies array, ratings array and number of movies. The function should process the ratings and print a message for each movie based on its rating:
+       - <MovieName>: "Excellent" for ratings 8 and above.
+       - <MovieName>: "Worth Watching" for ratings between 5 and 7.
+       - <MovieName>: "Better Skip" for ratings below 5.
+    5. Ensure proper memory management by deallocating the memory once it is no longer needed.
+
+    */
+void sortMovieRatings(std::string *movie, float *rating, int size) {
+  int topRatings[size];
+  int mediumRatings[size];
+  int lowRatings[size];
+  for (int i = 0; i < size; i++) {
+    if (rating[0] >= 8) {
+      topRatings[size]
+    }
+  }
 }
 
 int main() {
-  int x = 5;
-  int y = 10;
-  cout << "Before Swap: x=" << x << " y=" << y << endl;
-  swap(x,y);
-  cout << "After Swap: x=" << x << " y=" << y << endl;
-  return 0;
+  int size;
+  std::string *movieName;
+  float *movieRating;
+  cout << "How many movies would you like to add" << endl;
+  cin >> size;
+  movieName= new int[size];
+  movieRating = new int[size];
+  for (int i = 0; i < size; i++) {
+    cout << "Add the name of the movie" << endl;
+    cin >> movieName[i];
+    cout << "Add the rating of the movie" << endl;
+    cin >> movieRating[i];
+  }
+  sortMovieRatings(movieName,movieRating,size);
 }
