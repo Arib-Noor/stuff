bd1e47991e0272637e312dbfb2b2b15ebde26c51
Something <anoor39@myseneca.ca>
2026-02-19 02:26:33 +0000

    E

--- main.cpp
+++ main.cpp
@@ -1,29 +1,18 @@
-//Lab 4 Task 1
+//Lab 5 Task 2
 #include <iostream>  
 using namespace std;
-/*
-* Add necessary include files, use std namespace, and create the skeleton of main function.
-* Declare an integer variable `size`, and a float* pointer `scores`, initialized to `nullptr`.
-* Ask the user to enter the number of students and store it in `size`.
-* Dynamically allocate memory for a float array of that size using the statement:
-  ```cpp
-   scores = new float[size];
-  ```
-* Print a message confirming that memory was allocated.
-* Initialize the array with fixed values (e.g., {85.5, 90.0, 78.5, 92.0, 88.0}).
-* Use a loop to display the values.
-* Free the memory using delete[].
 
-*/
+void swap(int &a, int &b) {
+  int temp = a;
+  a = b;
+  b = temp;
+}
+
 int main() {
-  int size;
-  float *scores = nullptr;
-  cout << "Enter an integer " << endl;
-  cin >> size;
-  scores = new float[size];
-  cout << "Memory allocated" << endl;
-  scores = {1.0F,1.0F};
-  for (int i = 0; i < size; i++) {  
-    cout << scores[i];
-  }
+  int x = 5;
+  int y = 10;
+  cout << "Before Swap: x=" << x << " y=" << y << endl;
+  swap(x,y);
+  cout << "After Swap: x=" << x << " y=" << y << endl;
+  return 0;
 }
